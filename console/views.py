5262091441e04fd6a57b6f08fc239a2d5beeaafc
narendra <narendrachouhan.engg@gmail.com>
2021-06-13 16:46:30 +0530

    registration with profile model

--- console/views.py
+++ console/views.py
@@ -1,5 +1,9 @@
 from django.shortcuts import render,redirect,reverse, get_object_or_404
 from django.contrib.auth import authenticate, login, logout 
+from console.forms import SignUpForm
+from django.contrib import messages
+from django.utils.translation import ugettext as _
+from django.http import HttpResponseRedirect
 
 # Create your views here.
 
@@ -10,4 +14,16 @@ def signout(request):
 
 
 def register(request):
-	return render(request,'console/register.html')
+	if request.method=='POST':
+		form = SignUpForm(request.POST)
+		if form.is_valid():
+			user = form.save()
+			user.refresh_from_db()
+			user.profile.genre = form.cleaned_data.get('genre')
+			user.profile.birth_date = form.cleaned_data.get('birth_date')
+			user.save()
+			messages.success(request,_('User registered successfully'))
+			return HttpResponseRedirect(reverse('login')) 			
+	else:
+		form = SignUpForm()
+	return render(request,'console/register.html',{'form':form})
