a24fc64b3c0ae351e8e2702201697ab4b7c6411c
narendra <narendrachouhan.engg@gmail.com>
2021-06-13 10:43:26 +0530

    crispyforms added

--- myjaan/settings.py
+++ myjaan/settings.py
@@ -40,8 +40,11 @@ INSTALLED_APPS = [
     'myapp',
     'console',
     'rest_framework',
-    'catalog'
+    'catalog',
+    'crispy_forms',
 ]
+
+CRISPY_TEMPLATE_PACK = 'bootstrap4'
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
